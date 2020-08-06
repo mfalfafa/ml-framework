0b40692ad2e5f54798afc15b0ec23513727bfa27
ff077 <miftahf77@gmail.com>
2020-08-06 07:11:39 +0700

    add files

--- src/create_folds.py
+++ src/create_folds.py
@@ -7,12 +7,12 @@ def get_args():
 	parser=argparse.ArgumentParser('Folds generator')
 	parser.add_argument('-f', '--csv_filename', type=str, default=None, help='csv file path')
 	parser.add_argument('-t', '--target_column', type=str, default=None, help='target column for kfold')
-	parser.add_argument('s', '--n_splits', type=int, default=5, help='number of kfold')
+	parser.add_argument('-s', '--n_splits', type=int, default=5, help='number of kfold')
 	args=parser.parse_args()
 	return args
 
 def create_folds(opt):
-	df=pd.read_csv(os.path.join('../input/', opt.filename))
+	df=pd.read_csv(os.path.join('input/', opt.filename))
 	df['kfold']=-1
 	df=df.sample(frac=1).reset_index(drop=True)
 	y=df[opt.target_column]
@@ -21,7 +21,7 @@ def create_folds(opt):
 		df.loc[v_, 'kfold']=f
 
 	filename=opt.filename.split('.')[0]+'_folds.csv'
-	df.to_csv(os.path.join('../input/', filename), index=False)
+	df.to_csv(os.path.join('input/', filename), index=False)
 
 if __name__=='__main__':
 	opt=get_args()
